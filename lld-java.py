4691c90a8c6dbe1ae817417d87fff5c34fdc4df8
djluo <dj.luo@baoyugame.com>
2015-09-11 09:19:50 +0800

    v1

--- lld-java.py
+++ lld-java.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# vim:set et ts=2 sw=2: #
 
 import sys
 import json
@@ -6,54 +7,91 @@ import struct
 import socket
 
 port = sys.argv[1]
-keys = sys.argv[2]
+
+if port == "discovery":
+  ports = { "data": [ 
+            { "{#PORT}": "9009", "{#NAME}": "h1-sso"   }, 
+            { "{#PORT}": "9100", "{#NAME}": "h1-sdk"   }, 
+            { "{#PORT}": "9000", "{#NAME}": "h1-inner" }, 
+            { "{#PORT}": "9001", "{#NAME}": "h1-login" },
+            { "{#PORT}": "9002", "{#NAME}": "h1-outer" }, 
+          ] }
+  print json.dumps(ports,indent=4)
+  sys.exit(0)
+else:
+  keys = sys.argv[2]
 
 JavaGateway = ('172.17.42.1', 10052)
 
 common  = {
   "request":  "java gateway jmx",
-  "conn":     "192.168.1.83",
+  "conn":     "192.168.1.82",
   "username": None,
   "password": None,
 }
 
 jmxs = {
-    "uptime"  : "jmx[java.lang:type=Runtime,Uptime]",
-    "maxfd"   : "jmx[java.lang:type=OperatingSystem,MaxFileDescriptorCount]",
-    "openfd"  : "jmx[java.lang:type=OperatingSystem,OpenFileDescriptorCount]",
-    "version" : "jmx[java.lang:type=Runtime,VmVersion]",
+    "Uptime"            : "jmx[java.lang:type=Runtime,Uptime]",
+    "MaxFD"             : "jmx[java.lang:type=OperatingSystem,MaxFileDescriptorCount]",
+    "OpenFD"            : "jmx[java.lang:type=OperatingSystem,OpenFileDescriptorCount]",
+    "Version"           : "jmx[java.lang:type=Runtime,VmVersion]",
     "ThreadCount"       : "jmx[java.lang:type=Threading,ThreadCount]",
     "PeakThreadCount"   : "jmx[java.lang:type=Threading,PeakThreadCount]",
     "DaemonThreadCount" : "jmx[java.lang:type=Threading,DaemonThreadCount]",
+    "GC.CMS.Time"       : "jmx[\"java.lang:type=GarbageCollector,name=ConcurrentMarkSweep\",CollectionTime]",
+    "GC.CMS.Count"      : "jmx[\"java.lang:type=GarbageCollector,name=ConcurrentMarkSweep\",CollectionCount]",
+    "GC.PSM.Time"       : "jmx[\"java.lang:type=GarbageCollector,name=PS MarkSweep\",CollectionTime]",
+    "GC.PSM.Count"      : "jmx[\"java.lang:type=GarbageCollector,name=PS MarkSweep\",CollectionCount]",
+    "GC.PSS.Time"       : "jmx[\"java.lang:type=GarbageCollector,name=PS Scavenge\", CollectionTime]",
+    "GC.PSS.Count"      : "jmx[\"java.lang:type=GarbageCollector,name=PS Scavenge\", CollectionCount]",
+    "GC.PN.Time"        : "jmx[\"java.lang:type=GarbageCollector,name=ParNew\",CollectionTime]",
+    "GC.PN.Count"       : "jmx[\"java.lang:type=GarbageCollector,name=ParNew\",CollectionCount]",
+    "GC.COPY.Time"      : "jmx[\"java.lang:type=GarbageCollector,name=Copy\",  CollectionTime]",
+    "GC.COPY.Count"     : "jmx[\"java.lang:type=GarbageCollector,name=Copy\",  CollectionCount]",
+    "MP.CC.max"         : "jmx[\"java.lang:type=MemoryPool,name=Code Cache\",    Usage.max]",
+    "MP.CC.used"        : "jmx[\"java.lang:type=MemoryPool,name=Code Cache\",    Usage.used]",
+    "MP.CC.committed"   : "jmx[\"java.lang:type=MemoryPool,name=Code Cache\",    Usage.committed]",
+    "MP.PSOG.max"       : "jmx[\"java.lang:type=MemoryPool,name=PS Old Gen\",    Usage.max]",
+    "MP.PSOG.used"      : "jmx[\"java.lang:type=MemoryPool,name=PS Old Gen\",    Usage.used]",
+    "MP.PSOG.committed" : "jmx[\"java.lang:type=MemoryPool,name=PS Old Gen\",    Usage.committed]",
+    "MP.PSES.max"       : "jmx[\"java.lang:type=MemoryPool,name=PS Eden Space\",    Usage.max]",
+    "MP.PSES.used"      : "jmx[\"java.lang:type=MemoryPool,name=PS Eden Space\",    Usage.used]",
+    "MP.PSES.committed" : "jmx[\"java.lang:type=MemoryPool,name=PS Eden Space\",    Usage.committed]",
+    "MP.PSSS.max"       : "jmx[\"java.lang:type=MemoryPool,name=PS Survivor Space\",Usage.max]",
+    "MP.PSSS.used"      : "jmx[\"java.lang:type=MemoryPool,name=PS Survivor Space\",Usage.used]",
+    "MP.PSSS.committed" : "jmx[\"java.lang:type=MemoryPool,name=PS Survivor Space\",Usage.committed]",
+    "M.HEAP.max"        : "jmx[java.lang:type=Memory,HeapMemoryUsage.max]",
+    "M.HEAP.used"       : "jmx[java.lang:type=Memory,HeapMemoryUsage.used]",
+    "M.HEAP.committed"  : "jmx[java.lang:type=Memory,HeapMemoryUsage.committed]",
     }
+def queries():
+  common["keys"] = [ jmxs[keys] ]
+  common["port"] = port
 
-common["keys"] = [ jmxs[keys] ]
-common["port"] = port
+  queries = json.dumps(common)
 
-queries = json.dumps(common)
+  header = 'ZBXD\1'
+  length = struct.pack('<8B', len(queries),0,0,0,0,0,0,0)
 
-header = 'ZBXD\1'
-length = struct.pack('<8B', len(queries),0,0,0,0,0,0,0)
+  s = None
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect(JavaGateway)
 
-s = None
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(JavaGateway)
-
-if s is None:
+  if s is None:
     print 'could not open socket'
     sys.exit(1)
 
-s.send(header)
-s.send(length)
-s.send(queries)
+  s.send(header)
+  s.send(length)
+  s.send(queries)
 
-data = s.recv(5)
-data = s.recv(8)
-data = s.recv(1024)
-s.close()
+  data = s.recv(5)
+  data = s.recv(8)
+  data = s.recv(1024)
+  s.close()
 
-value = json.loads(data)
-print repr(value)
-value = value["data"][0]
+  value = json.loads(data)
+  value = value["data"][0]
+  print value.get("value")
 
-print value.get("value")
+queries()
